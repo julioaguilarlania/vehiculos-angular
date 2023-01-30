302d8a113175b932a95619b34bc08b3033e46db3
jaguilar <julio.aguilar@lania.edu.mx>
2023-01-30 16:03:38 -0600

    Nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaVehiculosComponent } from './lista-vehiculos/lista-vehiculos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaVehiculosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lista-vehiculos/lista-vehiculos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-vehiculos',
+  templateUrl: './lista-vehiculos.component.html',
+  styleUrls: ['./lista-vehiculos.component.css']
+})
+export class ListaVehiculosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
