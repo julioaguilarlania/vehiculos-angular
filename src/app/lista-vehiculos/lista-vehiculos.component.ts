ca464280e1d43a3ee9b34453aee198ab6ca7a7b2
jaguilar <julio.aguilar@lania.edu.mx>
2023-01-30 21:13:16 -0600

    Ejemplos básicos

--- src/app/lista-vehiculos/lista-vehiculos.component.ts
+++ src/app/lista-vehiculos/lista-vehiculos.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaVehiculosComponent implements OnInit {
 
+  mensaje: string = "Hola mundo desde Angular";
+  activo: boolean = true;
+  lista: string[] = ["Fulano", "Mengano", "Perengano"];
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log('ngOnOnit()');
+  }
+
+  toggleActivo():void {
+    this.activo = !this.activo;
   }
 
 }
