9ea62296d3d8875f3d8aa2c577aa022611b0213c
jaguilar <julio.aguilar@lania.edu.mx>
2023-01-30 21:13:01 -0600

    Mapeo de nuevo componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ListaVehiculosComponent } from './lista-vehiculos/lista-vehiculos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'vehiculos',
+    component: ListaVehiculosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
