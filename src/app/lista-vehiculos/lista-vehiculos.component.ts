d66abdbd5cc6d8c9c6d10ce9ace404d237997bde
jaguilar <julio.aguilar@lania.edu.mx>
2023-01-31 13:36:30 -0600

    Obtiene lista estatica desde servicios

--- src/app/lista-vehiculos/lista-vehiculos.component.ts
+++ src/app/lista-vehiculos/lista-vehiculos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from '../models/vehiculo.model';
+import { VehiculosService } from '../services/vehiculos.service';
 
 @Component({
   selector: 'app-lista-vehiculos',
@@ -10,11 +12,13 @@ export class ListaVehiculosComponent implements OnInit {
   mensaje: string = "Hola mundo desde Angular";
   activo: boolean = true;
   lista: string[] = ["Fulano", "Mengano", "Perengano"];
+  listaVehiculos: Vehiculo[] = [];
 
-  constructor() { }
+  constructor(private vsService:VehiculosService) { }
 
   ngOnInit(): void {
     console.log('ngOnOnit()');
+    this.listaVehiculos = this.vsService.getVehiculos();
   }
 
   toggleActivo():void {
--- src/app/services/vehiculos.service.ts
+++ src/app/services/vehiculos.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Vehiculo } from '../models/vehiculo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehiculosService {
 
+  private listaVehiculos:Vehiculo[] = [
+    {
+      placas:'POE0928',
+      marca:'M1',
+      modelo:'M2022',
+      color:'Azul',
+      kilometraje:3892
+    },
+    {
+      placas:'PWE9128',
+      marca:'M2',
+      modelo:'N2021',
+      color:'Verde',
+      kilometraje:98786
+    },
+  ];
+
   constructor() { }
+
+  getVehiculos():Vehiculo[] {
+    return this.listaVehiculos;
+  }
 }
